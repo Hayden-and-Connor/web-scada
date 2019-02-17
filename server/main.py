746c306f70060b79abc960a8684c051c339ee076
Connor Winiarczyk <winiarcc@ad.lafayette.edu>
2019-02-17 01:21:16 -0500

    Flask RnD

--- server/main.py
+++ server/main.py
@@ -1,6 +1,13 @@
 from flask import Flask
-app = Flask(__main__)
+from flask import request
+
+app = Flask(__name__)
 
 @app.route('/')
-def hello():
-	return 'Hello World'
+def index():
+	data = request.args.get('data')
+	return f'Hello World {data}'
+
+@app.route('/another')
+def another():
+	return 'test'
