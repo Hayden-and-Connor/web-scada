087724d114eb356480b49c7a9e6e9f2280e08ffc
dodgeh <dodgeh@lafayette.edu>
2019-02-20 00:31:28 -0500

    added init for MotorController lister subclass.

--- can/testdecode.py
+++ can/testdecode.py
@@ -26,21 +26,21 @@ class MotorController(can.Listener):
 	"""
 	#TODO add full motorcontroller response to CANbus messages
 	# add ability to reconfigure motorcontroller simulator
-	# def __init__(self):
-	# 	return
+	def __init__(self, bus):
+		self.bus = bus
 
 	def on_message_received(self, msg):
 		if msg.arbitration_id == 0x80 and msg.data == 0x00:
 			#TODO add more PDOs
 			#TODO add feature to count syncs before sending PDO
-			self.send(PDO1)
+			self.bus.send(PDO1)
 		# Add more CANopen responses here
 
 mc = MotorController()
 notifier = can.Notifier(mc_bus, [mc])		
 
 sync = can.Message(arbitration_id=0x80, data=0x00)
-sync_bus.send_periodic(sync, 0.1)
+sync_bus.send_periodic(sync, 1)
 
 for msg in scada_bus:
 	print(msg)
