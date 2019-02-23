923a039094e697dbd95f9bb9b802e29745bf1c9e
dodgeh314 <dodgeh@lafayette.edu>
2019-02-23 14:00:00 -0500

    syntaax error removed class

--- dbwriter.py
+++ dbwriter.py
@@ -3,14 +3,13 @@ from pyee import EventEmitter
 import sqlite3
 from sqlite3 import Error
 
-class DBWriter:
-
 def setup(dbfile, event_emitter, event_type):
     # check that db exists if not create one
 
 
     @event_emitter.on(event_type)
     def save_data(name, val, ts):
+        print val
         conn = create_connection(dbfile)
         with conn:
             create_data(conn, (name, val, ts))
